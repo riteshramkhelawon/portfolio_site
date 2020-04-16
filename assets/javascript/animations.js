3e607659116c0f3611f2cb86602a1a6c9ecd3697
riteshramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-04-16 17:40:50 +0100

    Moved animation JS code into a separate file

---
+++ assets/javascript/animations.js
@@ -0,0 +1,139 @@
+ window.sr = ScrollReveal();
+
+        //NAVBAR
+        sr.reveal('#myNavbar', {
+            duration: 2000,
+            origin: 'bottom',
+        });
+
+        //ABOUT SECTION
+        sr.reveal('#profile-pic', {
+            duration: 3000,
+            origin: 'top',
+            distance: '100px'
+        });
+        sr.reveal('.profile-basic-info', {
+            duration: 3000,
+            origin: 'bottom',
+            distance: '50px'
+        });
+        sr.reveal('#profile-text', {
+            duration: 4000,
+            origin: 'bottom',
+            distance: '50px'
+        });
+        sr.reveal('.profile-hr', {
+            duration: 2000,
+            delay: 500,
+            origin: 'bottom',
+            distance: '50px'
+        });
+        sr.reveal('#socialmedialinks', {
+            duration: 2000,
+            delay: 550,
+            origin: 'bottom',
+            distance: '50px'
+        });
+
+        //SKILLS SECTION
+        sr.reveal('.skills-heading', {
+            duration: 3000,
+            origin: 'bottom',
+            distance: '50px'
+        });
+        sr.reveal('.skills-title', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '100px'
+        });
+        sr.reveal('.frontEnd', {
+            duration: 2000,
+            delay: 200,
+            origin: 'bottom',
+            distance: '100px'
+        });
+        sr.reveal('.backEnd', {
+            duration: 2000,
+            delay: 200,
+            origin: 'bottom',
+            distance: '100px'
+        });
+        sr.reveal('.otherTools', {
+            duration: 2000,
+            delay: 200,
+            origin: 'bottom',
+            distance: '100px'
+        });
+        sr.reveal('.skills-hr', {
+            duration: 2000,
+            delay: 350,
+            origin: 'bottom',
+            distance: '100px'
+        });
+        sr.reveal('.downloadCV', {
+            duration: 2000,
+            delay: 400,
+            origin: 'bottom',
+            distance: '100px'
+        });
+
+        //WORK SECTION
+        sr.reveal('.work-heading', {
+            duration: 3000,
+            origin: 'bottom',
+            distance: '50px'
+        });
+        sr.reveal('.projectwork', {
+            duration: 2000,
+            delay: 200,
+            origin: 'bottom',
+            distance: '100px'
+        });
+        sr.reveal('.project-title', {
+            duration: 2000,
+            delay: 500,
+            origin: 'bottom',
+            distance: '100px'
+        });
+
+        //CONTACT SECTION
+        sr.reveal('.contact-heading', {
+            duration: 3000,
+            origin: 'bottom',
+            distance: '50px'
+        });
+        sr.reveal('.contact-text', {
+            duration: 2000,
+            delay: 200,
+            origin: 'bottom',
+            distance: '100px'
+        });
+        sr.reveal('.contact-name', {
+            duration: 2000,
+            delay: 200,
+            origin: 'bottom',
+            distance: '100px'
+        });
+        sr.reveal('.contact-email', {
+            duration: 2000,
+            delay: 250,
+            origin: 'bottom',
+            distance: '100px'
+        });
+        sr.reveal('.contact-subject', {
+            duration: 2000,
+            delay: 300,
+            origin: 'bottom',
+            distance: '100px'
+        });
+        sr.reveal('.contact-message', {
+            duration: 2000,
+            delay: 350,
+            origin: 'bottom',
+            distance: '100px'
+        });
+        sr.reveal('.contact-send-btn', {
+            duration: 3000,
+            delay: 750,
+            origin: 'bottom',
+        });
